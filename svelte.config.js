0275e4afb32b43797efa342bb8b31e20e17e202c
cryptodeal <jimmydeal16@gmail.com>
2021-10-27 20:08:54 -0500

    add vercel adaptor; init deployment of demo

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import WindiCSS from 'vite-plugin-windicss';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
